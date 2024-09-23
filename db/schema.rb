238fee20123d94f9c857cd163a572de4cf62cae6
Fran <Franph2010@gmail.com>
2024-09-22 22:21:26 -0600

    Aplicacion con ruby y rails Git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_23_035817) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_23_041854) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
